55039686ed71003c6d24b067daae4110da503592
Junior Cercado Vasquez <juniorcercado@upeu.edu.pe>
2022-03-20 20:27:11 -0500

    new pantilla font

---
+++ head.php
@@ -0,0 +1,11 @@
+<!-- Favicon -->
+<link rel="shortcut icon" href="assets/svg/logos/logo-short.svg">
+
+<!-- Font -->
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&amp;display=swap" rel="stylesheet">
+
+<!-- CSS Implementing Plugins -->
+<link rel="stylesheet" href="assets/css/vendor.min.css">
+
+<!-- CSS Front Template -->
+<link rel="stylesheet" href="assets/css/theme.minc619.css?v=1.0">
---
+++ script.php
@@ -0,0 +1,26 @@
+<!-- Go to Top -->
+<a class="js-go-to go-to position-fixed" href="javascript:;" style="visibility: hidden;"
+    data-hs-go-to-options='{
+      "offsetTop": 700,
+      "position": {
+        "init": {
+          "right": 15
+        },
+        "show": {
+          "bottom": 15
+        },
+        "hide": {
+          "bottom": -15
+        }
+      }
+    }'>
+  <i class="fas fa-angle-up"></i>
+</a>
+<!-- End Go to Top -->
+
+
+<!-- JS Implementing Plugins -->
+<script src="assets/js/vendor.min.js"></script>
+
+<!-- JS Front -->
+<script src="assets/js/theme.min.js"></script>
